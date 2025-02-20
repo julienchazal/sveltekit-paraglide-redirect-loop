807d40080b02e47fbf77f253af59688678697d96
Julien CHAZAL <julien.chazal@wiseed.com>
2025-02-20 17:04:02 +0100

    remove redirect

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from '../../.svelte-kit/types/src/routes/$types';
 
 export const load: LayoutServerLoad = () => {
-		redirect(307, '/login');
+		// redirect(307, '/login');
 };
